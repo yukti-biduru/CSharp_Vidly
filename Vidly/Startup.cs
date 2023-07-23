5026ede9838c30191af25d626842c7f5e2153651
Yukti <yukti.biduru11@gmail.com>
2023-07-23 15:07:40 -0500

    Added individual authentication changes

---
+++ Vidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_with_Individual_Authentication.Startup))]
+namespace Vidly_with_Individual_Authentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
